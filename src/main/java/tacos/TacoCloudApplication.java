2d2436a83817f27bfd4aa9b464c0a7012f745172
Labhayam-Sharma <labhayamsharma@gmail.com>
2025-05-17 20:24:35 +0530

    Showcasing the real working of Sping Boot framework -> Annotations, Annotation Retention Policy and, Reflection API

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -5,15 +5,36 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TacoCloudApplication {
+	
 
-	public static void main(String[] args) throws NoSuchFieldException, SecurityException {
+	public static void main(String[] args) {
 		
+//		run();
+//		runController();
+		SpringApplication.run(TacoCloudApplication.class, args);
+
+		
+	}
+	
+/*	private static void runController(){
+		Class<?> cas = LandingPage.class;
+		GetMapping annotation = null;
+		try {
+			annotation = cas.getMethod("home", null).getAnnotation(GetMapping.class);
+		} catch (NoSuchMethodException | SecurityException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(annotation);
+		
+	}
+
+	public static void run() {
 		Class<?> c = TacoCloudApplication.class; // First, create an object of class Class of the java.lang package
 		SpringBootApplication cls = c.getAnnotation(SpringBootApplication.class);
 		System.out.println("Well Hello!" + cls.toString());
-		
-		
-		SpringApplication.run(TacoCloudApplication.class, args);
+
 	}
+	*/
 	
 }
